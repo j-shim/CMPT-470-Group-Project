5c46e9f6caaca192cc7491e0411ab25ef510066f
June Shim <jys2@sfu.ca>
2020-07-24 21:41:29 -0700

    Add console.log/error messages for server's error-handler and MySQL connection

--- server/helpers/error-handler.js
+++ server/helpers/error-handler.js
@@ -1,6 +1,8 @@
 module.exports = errorHandler
 
 function errorHandler(err, req, res, next) {
+  console.error(err)
+
   if (typeof (err) === 'string') {
     // custom application error
     return res.status(400).json({ message: err })
@@ -8,14 +10,17 @@ function errorHandler(err, req, res, next) {
 
   if (err.name === 'ValidationError') {
     // mongoose validation error
+    console.error(err.message)
     return res.status(400).json({ message: err.message })
   }
 
   if (err.name === 'UnauthorizedError') {
     // jwt authentication error
+    console.error('Invalid Token')
     return res.status(401).json({ message: 'Invalid Token' })
   }
 
   // default to 500 server error
+  console.error(err.message)
   return res.status(500).json({ message: err.message })
 }
